763ffb51ce3c1e717fe440f30f3c2f7395d4ab68
DoHuyToan <dhtoan89@gmail.com>
2021-09-16 22:06:47 +0700

    Quyet tam

---
+++ Bai2/src/SoNguyenTo/Prime.java
@@ -0,0 +1,29 @@
+package SoNguyenTo;
+
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); //khai báo biến scanner
+        System.out.println("Điền số cần kiểm tra số nguyên tố vào");
+        int num = scanner.nextInt();
+        boolean check = true;
+
+        if(num<2){
+            check = false;
+        } else {
+            int i =2;
+            while (i<=Math.sqrt(num)){
+                if(num % i == 0){
+                    check = false;
+                    break;
+                } i++;
+            }
+
+        }
+        if(check)
+            System.out.println(num + " is prime");
+        else
+            System.out.println(num + " not is prime");
+    }
+}
