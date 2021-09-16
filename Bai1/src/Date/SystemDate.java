3e2a81202b085c935ce860c79db090b279970df6
DoHuyToan <dhtoan89@gmail.com>
2021-09-16 10:27:11 +0700

    add file

---
+++ Bai1/src/Date/SystemDate.java
@@ -0,0 +1,11 @@
+package Date;
+
+import java.util.Date;
+
+    public class SystemDate {
+        public static void main(String[] args) {
+            Date now = new Date();
+            System.out.println("Now is: " + now);
+        }
+    }
+
--- Bai1/src/SystemTime.java
+++ Bai1/src/SystemTime.java
@@ -1,9 +1,4 @@
 import java.util.Date;
 
-public class SystemTime {
-    public static void main(String[] args) {
-        Date now = new Date();
-        System.out.println("Now is: " + now);
 
-    }
 }
