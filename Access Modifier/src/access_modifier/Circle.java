e0631228099bec720c6214e60f89ab3666f94499
DoHuyToan <dhtoan89@gmail.com>
2021-09-21 23:27:42 +0700

    Quyet tam

--- Access Modifier/src/access_modifier/Circle.java
+++ Access Modifier/src/access_modifier/Circle.java
@@ -1,7 +1,7 @@
 package access_modifier;
 
 public class Circle {
-    private double radius = 5.0;
+    private double radius = 1.0;
     private String color = "red";
 
     public Circle(){}
--- Access Modifier/src/access_modifier/TestCircle.java
+++ Access Modifier/src/access_modifier/TestCircle.java
@@ -3,7 +3,7 @@ package access_modifier;
 public class TestCircle {
     public static void main(String[] args) {
         Circle circle = new Circle();
-        circle.getRadius();
+        circle.setRadius(5);
         circle.getArea();
         circle.setColor("Green");
         System.out.print(circle.toString());
