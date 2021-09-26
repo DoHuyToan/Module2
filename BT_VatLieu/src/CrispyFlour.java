5cb4059afe303f570b144284ae3ce99c9327509d
DoHuyToan <dhtoan89@gmail.com>
2021-09-26 17:12:41 +0700

    Chua hoan thanh

--- BT_VatLieu/src/CrispyFlour.java
+++ BT_VatLieu/src/CrispyFlour.java
@@ -1,6 +1,6 @@
 import java.time.LocalDate;
 
-public class CrispyFlour extends Material{
+public class CrispyFlour extends Material implements Discount{
     private double quantity;
 
     public CrispyFlour() {
@@ -19,6 +19,19 @@ public class CrispyFlour extends Material{
     @Override
     public LocalDate getExpiryDate() {
         return getMfg().plusYears(1);
+    }
+
+    @Override
+    public double getRealMoney() {
+        LocalDate nowlDate = LocalDate.now();
+        if((getExpiryDate().equals(nowlDate.plusMonths(4))) || getExpiryDate().isBefore(nowlDate.plusMonths(4))){
+            return getAmount()*80/100;
+        } else {
+            if ((getExpiryDate().equals(nowlDate.plusMonths(2))) || getExpiryDate().isBefore(nowlDate.plusMonths(2))){
+                return getAmount()*60/100;
+            }
+            else return getAmount()*95/100;
+        }
     }
 
     @Override
